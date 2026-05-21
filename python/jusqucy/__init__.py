f3af2a3af21d801f2ecde8ddcf1fb781792a4bf8
thjbbvlt <thibault2@ik.me>
2026-05-21 13:45:14 +0200

    minor change
    remove useless commented code

--- python/jusqucy/__init__.py
+++ python/jusqucy/__init__.py
@@ -1,10 +1,1 @@
 from jusqucy.jusqucy import tokenize, ttypify
-
-# from jusqucy.ttypes import TokenType
-#
-# try:
-#     import jusqucy.tokenizer
-#     from .tokenizer import JusqucyTokenizer
-#
-# except ModuleNotFoundError:
-#     pass
